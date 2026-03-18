c710f66f9780e49e4185fb08caf0c24987f4d64b
qubonic <mjfumiten@antiquespride.edu.ph>
2026-03-18 12:36:16 +0800

    chore: raleway and more sugar fonts added

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<main class="responsive">
+	<div class="rfc-hero-banner center-align">
+		<nav>
+			<button class="circle transparent large">
+				<i>account_circle</i>
+			</button>
+
+			<div class="max"></div>
+
+			<button class="circle transparent large">
+				<i>settings</i>
+			</button>
+		</nav>
+
+		<img src="" alt="" />
+		<h1>Rafico</h1>
+	</div>
+
+	<div class="rfc-menu center-align">
+		<div class="padding grid">
+			<button class="primary-container s12 m4 large">Journals</button>
+			<button class="primary-container s12 m4 large">Chat</button>
+			<button class="primary-container s12 m4 large">Music</button>
+		</div>
+	</div>
+</main>
